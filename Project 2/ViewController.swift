c47660c7a1f37d1647e2af9da240b47bc2524ad0
leeal <leeal@leealtekiMacBook-Air.local>
2017-12-11 00:55:46 +0800

    first commit

--- Project 2/ViewController.swift
+++ Project 2/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomNumber1 : Int = 0
+    var randomNumber2 : Int = 0
+    let imageArray = ["1","2","3","4","5","6"]
 
+    @IBOutlet weak var diceImage1: UIImageView!
+    @IBOutlet weak var diceImage2: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       updateDiceImage()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func Roll(_ sender: UIButton) {
+        updateDiceImage()
+       
+    }
+    
+    func updateDiceImage(){
+        randomNumber1 = Int(arc4random_uniform(6))
+        randomNumber2 = Int(arc4random_uniform(6))
+        diceImage1.image = UIImage(named:imageArray[randomNumber1])
+        diceImage2.image = UIImage(named:imageArray[randomNumber2])
+    }
 }
 
